1cedbe58fe5bee59de190064f88611b7784ea1f6
Patrick Ouellet <ouellet.patrick@gmail.com>
2024-02-17 17:31:06 -0500

    Added comments

--- CoffeeMachine/main.py
+++ CoffeeMachine/main.py
@@ -80,7 +80,7 @@ def get_coffee_cost(drink_type):
 def calculate_money(quarters, dimes, nickels, pennies):
     return (quarters * 0.25) + (dimes * 0.1) + (nickels * 0.05) + (pennies * 0.01)
 
-
+# Main code
 machine_on = True
 while machine_on:
     machine_action = input("What would you like? (espresso/latte/cappuccino): ")

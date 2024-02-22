5a6294aece80a4ee0bb2302248a6207fd7a827d1
Patrick Ouellet <ouellet.patrick@gmail.com>
2024-02-22 09:19:13 -0500

    Day 20 commit

---
+++ Day20 - SnakeGame/main.py
@@ -0,0 +1,30 @@
+from turtle import Screen
+from snake import Snake
+import time
+
+screen = Screen()
+screen.setup(600, 600)
+screen.bgcolor("black")
+screen.title("Snake Game 3000")
+screen.tracer(0)
+
+# Create snake
+snake = Snake()
+
+screen.listen()
+screen.onkey(snake.up,"Up")
+screen.onkey(snake.down,"Down")
+screen.onkey(snake.left, "Left")
+screen.onkey(snake.right, "Right")
+
+# Move snakes
+game_is_on = True
+while game_is_on:
+    screen.update()
+    time.sleep(0.1)
+
+    snake.move()
+
+
+
+screen.exitonclick()
---
+++ Day20 - SnakeGame/snake.py
@@ -0,0 +1,48 @@
+"""Models the machine that makes the coffee"""
+from turtle import Turtle
+
+STARTING_POSITIONS = [(0, 0), (-20, 0), (-40, 0)]
+UP = 90
+DOWN = 270
+LEFT = 180
+RIGHT = 0
+
+class Snake:
+    def __init__(self):
+
+        # Create snakes
+        self.timmys = []
+        self.create_snake()
+        self.head = self.timmys[0]
+
+    def create_snake(self):
+        for position in STARTING_POSITIONS:
+            new_timmy = Turtle(shape="square")
+            new_timmy.color("white")
+            new_timmy.penup()
+            new_timmy.speed("fastest")
+            new_timmy.goto(position)
+            self.timmys.append(new_timmy)
+
+    def move(self):
+        for timmy in range(len(self.timmys) - 1, 0, -1):
+            new_x = self.timmys[timmy - 1].xcor()
+            new_y = self.timmys[timmy - 1].ycor()
+            self.timmys[timmy].goto(new_x, new_y)
+        self.head.forward(20)
+
+    def up(self):
+        if self.head.heading() != DOWN:
+            self.head.setheading(UP)
+
+    def down(self):
+        if self.head.heading() != UP:
+            self.head.setheading(DOWN)
+
+    def left(self):
+        if self.head.heading() != RIGHT:
+            self.head.setheading(LEFT)
+
+    def right(self):
+        if self.head.heading() != LEFT:
+            self.head.setheading(RIGHT)

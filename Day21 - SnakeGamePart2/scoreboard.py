e43b8c94be6b32b4f1dbfc4c6023c6ba19c0875c
Patrick Ouellet <ouellet.patrick@gmail.com>
2024-02-26 08:43:24 -0500

    Day 24 - Add high score saved to file to Snake Game

--- Day21 - SnakeGamePart2/scoreboard.py
+++ Day21 - SnakeGamePart2/scoreboard.py
@@ -12,18 +12,29 @@ class Scoreboard(Turtle):
         self.speed("fastest")
         self.color("white")
         self.score = 0
+        self.get_high_score()
         self.goto(0,270)
         self.update_scoreboard()
 
     def update_scoreboard(self):
         self.clear()
-        self.write(f"Score: {self.score}", align=ALIGNMENT, font=FONT)
+        self.write(f"Score: {self.score} High Score: {self.high_score}", align=ALIGNMENT, font=FONT)
 
     def addpoint(self):
         self.score += 1
         self.update_scoreboard()
 
-    def gameover(self):
-        self.goto(0,0)
-        self.write("GAME OVER", align=ALIGNMENT, font=FONT)
+    def reset(self):
+        self.update_high_score()
+        self.score = 0
+        self.get_high_score()
+        self.update_scoreboard()
 
+    def get_high_score(self):
+        with open("data.txt") as file:
+            self.high_score = int(file.read())
+
+    def update_high_score(self):
+        if self.score > self.high_score:
+            with open("data.txt", mode="w") as file:
+                file.write(str(self.score))

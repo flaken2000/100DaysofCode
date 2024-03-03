ce194f1863bba81247803d61924420a13ef4b1a5
Patrick Ouellet <ouellet.patrick@gmail.com>
2024-03-03 12:38:20 -0500

    Day 30 commit

--- Day26 - NATO Alphabet/main.py
+++ Day26 - NATO Alphabet/main.py
@@ -4,6 +4,13 @@ data = pandas.read_csv("nato_phonetic_alphabet.csv")
 alpha = {row.letter: row.code for (index, row) in data.iterrows()}
 print(alpha)
 
-user_word = input("Enter a word: ").upper()
-new_list = [alpha[letter] for letter in user_word]
-print(new_list)
+ValidEntry = False
+while not ValidEntry:
+    user_word = input("Enter a word: ").upper()
+    try:
+        new_list = [alpha[letter] for letter in user_word]
+    except KeyError:
+        print("Only letters in the alphabet please")
+    else:
+        ValidEntry = True
+        print(new_list)
--- Day29 - Password Manager/main.py
+++ Day29 - Password Manager/main.py
@@ -2,13 +2,14 @@ from tkinter import *
 from tkinter import messagebox
 from random import choice, randint, shuffle
 import pyperclip
+import json
 letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
 numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
 label_font = "arial"
 label_size = 10
 default_email = "email@domain.com"
-password_file = "data.txt"
+password_file = "data.json"
 
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
@@ -33,19 +34,57 @@ def save_password():
     website = website_entry.get()
     email = email_entry.get()
     password = password_entry.get()
+    new_data = {
+        website: {
+            "email": email,
+            "password": password
+        }
+    }
 
     if len(website) == 0 or len(email) == 0 or len(password) == 0:
         messagebox.showinfo(title="Empty fields", message="Please do not leave any empty fields")
     else:
-        is_ok = messagebox.askokcancel(title=website, message=f"These are the details entered: \n Email: {email}\n Password: {password}\n Is it ok to save?")
-        if is_ok:
-            saved_entry = website + " | " + email + " | " + password + "\n"
-            website_entry.delete(0, END)
-            email_entry.delete(0, END)
-            email_entry.insert(0, default_email)
-            password_entry.delete(0, END)
-            with open(password_file, mode="a") as passwords:
-                passwords.write(saved_entry)
+        saved_entry = website + " | " + email + " | " + password + "\n"
+        website_entry.delete(0, END)
+        email_entry.delete(0, END)
+        email_entry.insert(0, default_email)
+        password_entry.delete(0, END)
+
+        try:
+            with open(password_file, mode="r") as passwords:
+                # Reading existing data
+                data = json.load(passwords)
+        except FileNotFoundError:
+            with open(password_file, mode="w") as passwords:
+                json.dump(new_data, passwords, indent=4)
+        else:
+            # Updating existing data with new entry
+            data.update(new_data)
+            with open(password_file, mode="w") as passwords:
+                # Saving updated data
+                json.dump(data, passwords, indent=4)
+
+
+# ---------------------------- SEARCH PASSWORD ------------------------------- #
+
+def search_password():
+    try:
+        with open(password_file, mode="r") as passwords:
+            # Reading existing data
+            data = json.load(passwords)
+    except FileNotFoundError:
+        messagebox.showinfo(title="Error", message="File not found")
+    else:
+        try:
+            website = website_entry.get()
+            email = data[website]["email"]
+            password = data[website]["password"]
+        except KeyError:
+            messagebox.showinfo(title="Not found", message="No details for the website exists")
+        else:
+            messagebox.showinfo(title=website, message=f"Email: {email}\nPassword: {password}")
+
+
 
 # ---------------------------- UI SETUP ------------------------------- #
 
@@ -63,8 +102,10 @@ canvas.grid(column=1, row=0)
 # Website
 website_label = Label(text="Website:", font=(label_font, label_size, "normal"))
 website_label.grid(column=0, row=1)
-website_entry = Entry(width=35)
-website_entry.grid(column=1, row=1, columnspan=2, sticky="EW")
+website_entry = Entry(width=21)
+website_entry.grid(column=1, row=1, sticky="EW")
+search_pass_button = Button(text="Search", command=search_password)
+search_pass_button.grid(column=2, row=1, sticky="EW")
 website_entry.focus()
 
 # Email/Username
